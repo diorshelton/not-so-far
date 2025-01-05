ad3d415d89aaabb69fd23b5d50fb2942175d61f8
Dior.Shelton <dior.shelton@gmail.com>
2025-01-04 21:38:46 -0500

    format file

--- src/components/LocalInset.tsx
+++ src/components/LocalInset.tsx
@@ -1,15 +1,15 @@
 import { Box, Card, Container, Heading, Inset, Text } from "@radix-ui/themes";
 import planet from "../assets/planet.svg";
-import "../component-styles/localInsetStyles.css"
+import asteroid from "../assets/asteroid.svg";
+import "../component-styles/localInsetStyles.css";
 
 interface LocalInsetProps {
-	
 	/** ID of celestial body */
 	id: string;
 	/** Title of celestial body */
 	englishName: string;
 	/** celestial body type */
-	bodyType: "Comet" | "Planet" | "Asteroid" | "Dwarf Planet" | "Moon"| "Star";
+	bodyType: "Comet" | "Planet" | "Asteroid" | "Dwarf Planet" | "Moon" | "Star";
 	/** celestial body vol value */
 	volValue?: number;
 	/** celestial body vol exponent */
@@ -22,55 +22,63 @@ interface LocalInsetProps {
 
 	massExponent?: number;
 }
-const LocalInset = (
-	({
-		englishName,
-		bodyType,
-		volValue,
-		volExponent,
-		density,
-		massValue,
-		massExponent,
-	}: LocalInsetProps) => {
-		return (
-			<Box className="inset" minHeight="2rem">
-				<Container className="container" size={"4"}>
-					<Card size={"4"} className="inset-card">
-						<img
-							className="cb-svg"
-							src={planet}
-							alt="red planet"
-							style={{
-								display: "block",
-								width: "100%",
-								height: 120,
-							}}
-						/>
-						<Inset clip={"padding-box"}>
-							<Heading className="inset-heading" align={"center"}>
-								{englishName}
-							</Heading>
-							<Text className="inset-text" as="p" size="5">
-								Type: {bodyType}
-							</Text>
-							<Text className="inset-text" as="p" size="5">
-								Volume:{volValue}x10
-								<sup>{volExponent}</sup>
-								 km<sup>3</sup>
-							</Text>
-							<Text className="inset-text" as="p" size="5">
-								Density: {density} g/cm <sup>3</sup>
-							</Text>
-							<Text className="inset-text" as="p" size="5">
-								Mass: {massValue} x 10
-								<sup>{massExponent}</sup>
-								kg
-							</Text>
-						</Inset>
-					</Card>
-				</Container>
-			</Box>
-		);
-	}
-);
+const LocalInset = ({
+	englishName,
+	bodyType,
+	volValue,
+	volExponent,
+	density,
+	massValue,
+	massExponent,
+}: LocalInsetProps) => {
+
+	const setSvgImage = (bodyType: string) => {
+		let svgType = '';
+		for (const letter of bodyType) {
+			const char = letter.toLowerCase();
+			svgType += char
+		}
+		return svgType
+	};
+
+	return (
+		<Box className="inset" minHeight="2rem">
+			<Container className="container" size={"4"}>
+				<Card size={"4"} className="inset-card">
+					<img
+						className="cb-svg"
+						src={planet}
+						alt={setSvgImage(bodyType)}
+						style={{
+							display: "block",
+							width: "100%",
+							height: 120,
+						}}
+					/>
+					<Inset clip={"padding-box"}>
+						<Heading className="inset-heading" align={"center"}>
+							{englishName}
+						</Heading>
+						<Text className="inset-text" as="p" size="5">
+							Type: {bodyType}
+						</Text>
+						<Text className="inset-text" as="p" size="5">
+							Volume:{volValue}x10
+							<sup>{volExponent}</sup>
+							km<sup>3</sup>
+						</Text>
+						<Text className="inset-text" as="p" size="5">
+							Density: {density} g/cm <sup>3</sup>
+						</Text>
+						<Text className="inset-text" as="p" size="5">
+							Mass: {massValue} x 10
+							<sup>{massExponent}</sup>
+							kg
+						</Text>
+					</Inset>
+				</Card>
+			</Container>
+		</Box>
+	);
+};
 export default LocalInset;
