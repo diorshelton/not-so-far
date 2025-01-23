6be44f038110aa068aa53a280f2b3e83055d1d47
Dior.Shelton <dior.shelton@gmail.com>
2025-01-23 15:05:32 -0500

    configure server to redirect all non-static file requests.index.html and add file extention to GH config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/not-so-far',
+  base: '/not-so-far',
+  build: {rollupOptions:{input: 'index.html'}},
 })
