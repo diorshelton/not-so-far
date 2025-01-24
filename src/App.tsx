2f9eaa759e967bf462b04286ab4655db3dc07f28
Dior.Shelton <dior.shelton@gmail.com>
2025-01-23 19:49:42 -0500

    Remove code comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,86 @@
 import "./globals.css";
-// import LocalInset from "./components/LocalInset";
-// import LocalSelect from "./components/LocalSelect";
-// import Grid from "./components/Grid";
+import LocalInset from "./components/LocalInset";
+import LocalSelect from "./components/LocalSelect";
+import Grid from "./components/Grid";
 import Banner from "./components/Banner";
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-// /** celestial body volume */
-// interface Volume {
-// 	/** base vol value */
-// 	volValue: number;
-// 	/**exponent value */
-// 	volExponent: number;
-// }
-// /** celestial body Mass */
-// interface Mass {
-// 	/** base value */
-// 	massValue: number;
-// 	/**exponent value */
-// 	massExponent: number;
-// }
+/** celestial body volume */
+interface Volume {
+	/** base vol value */
+	volValue: number;
+	/**exponent value */
+	volExponent: number;
+}
+/** celestial body Mass */
+interface Mass {
+	/** base value */
+	massValue: number;
+	/**exponent value */
+	massExponent: number;
+}
 
-// type Body = {
-// 	/** ID of celestial body */
-// 	id: string;
-// 	/** English name*/
-// 	englishName: string;
-// 	/** celestial body type */
-// 	bodyType: "Comet" | "Planet" | "Asteroid" | "Dwarf Planet" | "Moon" | "Star";
-// 	/** celestial body volume */
-// 	vol?: Volume;
-// 	/** celestial body density */
-// 	density?: number;
-// 	/** celestial body mass */
-// 	mass?: Mass;
-// }[];
+type Body = {
+	/** ID of celestial body */
+	id: string;
+	/** English name*/
+	englishName: string;
+	/** celestial body type */
+	bodyType: "Comet" | "Planet" | "Asteroid" | "Dwarf Planet" | "Moon" | "Star";
+	/** celestial body volume */
+	vol?: Volume;
+	/** celestial body density */
+	density?: number;
+	/** celestial body mass */
+	mass?: Mass;
+}[];
 
 function App() {
-	// const [celestialBodies, setCelestialBodies] = useState<Body>([]);
-	// const [visibleBodies, setVisibleBodies] = useState<Body>([]);
-	// useEffect(() => {
-	// 	fetch("https://api.le-systeme-solaire.net/rest/bodies/")
-	// 		.then((response) => response.json())
-	// 		.then((data) => {
-	// 			setCelestialBodies(data.bodies);
-	// 			setVisibleBodies(data.bodies);
-	// 		})
-	// 		.catch((error) => {
-	// 			console.log(error);
-	// 		});
-	// }, []);
+	const [celestialBodies, setCelestialBodies] = useState<Body>([]);
+	const [visibleBodies, setVisibleBodies] = useState<Body>([]);
+	useEffect(() => {
+		fetch("https://api.le-systeme-solaire.net/rest/bodies/")
+			.then((response) => response.json())
+			.then((data) => {
+				setCelestialBodies(data.bodies);
+				setVisibleBodies(data.bodies);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
 
-	// const filterBodyByType = (filterBy: string) => {
-	// 	if (filterBy === "All") {
-	// 			return setVisibleBodies(celestialBodies)
-	// 		}
-	// 		const selectedType = celestialBodies.filter(
-	// 			(body) => body.bodyType === filterBy
-	// 		);
-	// 		setVisibleBodies(selectedType);
-	// 	};
+	const filterBodyByType = (filterBy: string) => {
+		if (filterBy === "All") {
+				return setVisibleBodies(celestialBodies)
+			}
+			const selectedType = celestialBodies.filter(
+				(body) => body.bodyType === filterBy
+			);
+			setVisibleBodies(selectedType);
+		};
 
-	// const selectHandler = (e:string) => {
-	// 	let alteredString: string = "";
-	// 	const bodyString = e.split("");
+	const selectHandler = (e:string) => {
+		let alteredString: string = "";
+		const bodyString = e.split("");
 
-	// 	bodyString.forEach((char: string, i: number) => {
-	// 		if (i > 0 && char === "p") {
-	// 			alteredString += char.toUpperCase();
-	// 		} else if (i === 0) {
-	// 			alteredString += char.toUpperCase();
-	// 		} else {
-	// 			alteredString += char;
-	// 		}
-	// 	});
+		bodyString.forEach((char: string, i: number) => {
+			if (i > 0 && char === "p") {
+				alteredString += char.toUpperCase();
+			} else if (i === 0) {
+				alteredString += char.toUpperCase();
+			} else {
+				alteredString += char;
+			}
+		});
 
-	// 	filterBodyByType(alteredString);
-	// };
+		filterBodyByType(alteredString);
+	};
 
 	return (
 		<>
 			<Banner />
-			{/* <LocalSelect checkBodyType={selectHandler} />
+			<LocalSelect checkBodyType={selectHandler} />
 			<Grid>
 				{visibleBodies.map((body) => {
 					return (
@@ -97,7 +97,7 @@ function App() {
 						/>
 					);
 				})}
-			</Grid> */}
+			</Grid>
 		</>
 	);
 }
--- src/components/LocalInset.tsx
+++ src/components/LocalInset.tsx
@@ -1,103 +1,103 @@
-// import { Box, Card, Container, Heading, Inset, Text } from "@radix-ui/themes";
-// import asteroid from "../assets/asteroid.svg";
-// import comet from "../assets/comet.svg";
-// import dwarfplanet from "../assets/dwarfplanet.svg";
-// import moon from "../assets/moon.svg";
-// import planet from "../assets/planet.svg";
-// import star from "../assets/star.svg";
-// import "../component-styles/localInsetStyles.css";
+import { Box, Card, Container, Heading, Inset, Text } from "@radix-ui/themes";
+import asteroid from "../assets/asteroid.svg";
+import comet from "../assets/comet.svg";
+import dwarfplanet from "../assets/dwarfplanet.svg";
+import moon from "../assets/moon.svg";
+import planet from "../assets/planet.svg";
+import star from "../assets/star.svg";
+import "../component-styles/localInsetStyles.css";
 
-// interface LocalInsetProps {
-// 	/** ID of celestial body */
-// 	id: string;
-// 	/** Title of celestial body */
-// 	englishName: string;
-// 	/** celestial body type */
-// 	bodyType: "Comet" | "Planet" | "Asteroid" | "Dwarf Planet" | "Moon" | "Star";
-// 	/** celestial body vol value */
-// 	volValue?: number;
-// 	/** celestial body vol exponent */
-// 	volExponent?: number;
-// 	/** celestial body density */
-// 	density?: number;
-// 	/** celestial body mass */
-// 	massValue?: number;
-// 	/** mass exponent */
+interface LocalInsetProps {
+	/** ID of celestial body */
+	id: string;
+	/** Title of celestial body */
+	englishName: string;
+	/** celestial body type */
+	bodyType: "Comet" | "Planet" | "Asteroid" | "Dwarf Planet" | "Moon" | "Star";
+	/** celestial body vol value */
+	volValue?: number;
+	/** celestial body vol exponent */
+	volExponent?: number;
+	/** celestial body density */
+	density?: number;
+	/** celestial body mass */
+	massValue?: number;
+	/** mass exponent */
 
-// 	massExponent?: number;
-// }
-// const LocalInset = ({
-// 	englishName,
-// 	bodyType,
-// 	volValue,
-// 	volExponent,
-// 	density,
-// 	massValue,
-// 	massExponent,
-// }: LocalInsetProps) => {
+	massExponent?: number;
+}
+const LocalInset = ({
+	englishName,
+	bodyType,
+	volValue,
+	volExponent,
+	density,
+	massValue,
+	massExponent,
+}: LocalInsetProps) => {
 
-// 	const setSvgAlt = (altText: string) =>{
-// 		return `drawing of ${altText}`
-// 	}
+	const setSvgAlt = (altText: string) =>{
+		return `drawing of ${altText}`
+	}
 
-// 	const setSvgSrc = (bodyType: string) => {
-// 		setSvgAlt(bodyType)
+	const setSvgSrc = (bodyType: string) => {
+		setSvgAlt(bodyType)
 
-// 		let svgString = "";
+		let svgString = "";
 
-// 		const svgTypes = {
-// 			Asteroid: asteroid,
-// 			Moon: moon,
-// 			Comet: comet,
-// 			Star: star,
-// 			DwarfPlanet: dwarfplanet,
-// 			Planet: planet,
-// 		};
+		const svgTypes = {
+			Asteroid: asteroid,
+			Moon: moon,
+			Comet: comet,
+			Star: star,
+			DwarfPlanet: dwarfplanet,
+			Planet: planet,
+		};
 
-// 		for (const char of bodyType) {
-// 			if (char !== " ") svgString += char;
-// 		}
-// 		return svgTypes[svgString];
-// 	};
+		for (const char of bodyType) {
+			if (char !== " ") svgString += char;
+		}
+		return svgTypes[svgString];
+	};
 
-// 	return (
-// 		<Box className="inset" minHeight="2rem">
-// 			<Container className="container" size={"4"}>
-// 				<Card size={"4"} className="inset-card">
-// 					<img
-// 						className="cb-svg"
-// 						src={setSvgSrc(bodyType)}
-// 						alt={setSvgAlt(bodyType)}
-// 						style={{
-// 							display: "block",
-// 							width: "100%",
-// 							height: 120,
-// 						}}
-// 					/>
-// 					<Inset clip={"padding-box"}>
-// 						<Heading className="inset-heading" align={"center"}>
-// 							{englishName}
-// 						</Heading>
-// 						<Text className="inset-text" as="p" size="5">
-// 							Type: {bodyType}
-// 						</Text>
-// 						<Text className="inset-text" as="p" size="5">
-// 							Volume:{volValue}x10
-// 							<sup>{volExponent}</sup>
-// 							km<sup>3</sup>
-// 						</Text>
-// 						<Text className="inset-text" as="p" size="5">
-// 							Density: {density} g/cm <sup>3</sup>
-// 						</Text>
-// 						<Text className="inset-text" as="p" size="5">
-// 							Mass: {massValue} x 10
-// 							<sup>{massExponent}</sup>
-// 							kg
-// 						</Text>
-// 					</Inset>
-// 				</Card>
-// 			</Container>
-// 		</Box>
-// 	);
-// };
-// export default LocalInset;
+	return (
+		<Box className="inset" minHeight="2rem">
+			<Container className="container" size={"4"}>
+				<Card size={"4"} className="inset-card">
+					<img
+						className="cb-svg"
+						src={setSvgSrc(bodyType)}
+						alt={setSvgAlt(bodyType)}
+						style={{
+							display: "block",
+							width: "100%",
+							height: 120,
+						}}
+					/>
+					<Inset clip={"padding-box"}>
+						<Heading className="inset-heading" align={"center"}>
+							{englishName}
+						</Heading>
+						<Text className="inset-text" as="p" size="5">
+							Type: {bodyType}
+						</Text>
+						<Text className="inset-text" as="p" size="5">
+							Volume:{volValue}x10
+							<sup>{volExponent}</sup>
+							km<sup>3</sup>
+						</Text>
+						<Text className="inset-text" as="p" size="5">
+							Density: {density} g/cm <sup>3</sup>
+						</Text>
+						<Text className="inset-text" as="p" size="5">
+							Mass: {massValue} x 10
+							<sup>{massExponent}</sup>
+							kg
+						</Text>
+					</Inset>
+				</Card>
+			</Container>
+		</Box>
+	);
+};
+export default LocalInset;
